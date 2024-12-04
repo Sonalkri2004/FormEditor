38bc89bfeb53db71fe36102e0abefcdf35df3cc3
Sonal kumari <115936469+Sonalkri2004@users.noreply.github.com>
2024-12-04 22:18:14 +0530

    routing setup.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { FormEditor } from './components/FormEditor';
+import { FormList } from './components/FormList';
+import { FormRenderer } from './components/FormRenderer';
+import { FormSuccess } from './components/FormSuccess';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className='bg-yellow-400'>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="min-h-screen bg-gray-100">
+      <Routes>
+        <Route path="/" element={<FormList />} />
+        <Route path="/create" element={<FormEditor />} />
+        <Route path="/form/:id" element={<FormRenderer />} />
+        <Route path="/success" element={<FormSuccess />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
